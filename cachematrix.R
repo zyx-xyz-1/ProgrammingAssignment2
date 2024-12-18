eba4d141a7316cdd5fd79933cb89b30029420400
zyx-xyz-1 <yzhu116@alumni.jh.edu>
2024-12-18 11:33:17 +0800

    fix some typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ##      The following two functions are used to create and cache the inverse 
-## matrix of any given invertible matrix. In this way, it avoids repeating 
-## calculate the inverses of matrices.
+## matrix of any given invertible matrix. In this way, it avoids repeating
+## to calculate the inverses of matrices.
 
 ##      For the first function, "makeCacheMatrix", it will return a list including
-## different functions, which allow users to set their matrices and get the
-## inverse form of their own matrices.
+## different functions, which creates the new matrix and allows users to set their 
+## matrices and get the inverse form of their own matrices.
 ## 1. set the matrix,
 ## 2. get the matrix,
 ## 3. set the inverse of the matrix,
@@ -25,11 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## In the second function, it calculate the inverse of the matrix created by the 
-## first function. Before the calculation, it first examine whether the inverse
-## matrix has been calculated and recorded in the first function. If so, it will
-## return the matrix and leave the message that "getting cached inverse matrix".
-## Otherwise, it will calculate the inverse matrix and cache it.
+##      In the second function, it calculates the inverse of the matrix created 
+## by the first function. Before the calculation, it first examines whether the 
+## inverse matrix has been calculated and recorded in the first function. If so,
+## it will return the matrix and leave the message that "getting cached inverse 
+## matrix". Otherwise, it will calculate the inverse matrix and cache it.
 
 cacheSolve <- function(x, ...) {
         inver<- x$get_solve()
